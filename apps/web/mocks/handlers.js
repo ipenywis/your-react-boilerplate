bd4027485aa39f54fe65afbe07b8257630302c82
Islem Maboud <islempenywis@gmail.com>
2023-01-07 15:17:32 +0100

    Add MSW for web

---
+++ apps/web/mocks/handlers.js
@@ -0,0 +1,7 @@
+import { rest } from "msw";
+export const handlers = [
+  // Handles a POST /login request
+  rest.post("/login", null),
+  // Handles a GET /user request
+  rest.get("/user", null),
+];
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,6 +3,12 @@ import "../globals.css";
 import { GlobalStyles } from "twin.macro";
 import { ChakraProvider } from "ui";
 
+/* eslint-disable global-require */
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  require("../mocks");
+}
+/* eslint-enable global-require */
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
